e539eb53a1019170b4d57aa0f0f6790388d583c2
SquallB <mazento2002@yahoo.fr>
2016-03-11 17:50:29 -0500

    centre de tirs sur le ship

--- src/Weapon.js
+++ src/Weapon.js
@@ -28,6 +28,8 @@ Weapon.SingleBullet.prototype.fire = function (source) {
 
     if (this.game.time.time < this.nextFire) { return; }
 
+    var x = source.x + 15;
+    var y = source.y + 15;
 
     this.getFirstExists(false).fire(x, y, 0, this.bulletSpeed, 0, 0);
 
