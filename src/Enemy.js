d93cad93ed3ae538b28835f700ce22d36f268a98
SquallB <mazento2002@yahoo.fr>
2016-03-11 16:31:44 -0500

    ajout des armes

--- src/Enemy.js
+++ src/Enemy.js
@@ -1,42 +1,10 @@
-Enemy = function(_game, _posX, _posY, _spriteName, _speed, _life, _bulletSpeed, _bulletDamage) {
-	Phaser.Sprite.call(this, _game, _posX, _posY, _spriteName);
+Enemy = function(game, posX, posY, spriteName, speed, life, bulletSpeed, bulletDamage) {
+	Phaser.Sprite.call(this, game, posX, posY, spriteName);
 
-	var speed = _speed || 0;
-	var life = _life || 0;
-	var bulletSpeed = _bulletSpeed || 0;
-	var bulletDamage = _bulletDamage || 0;
-
-	this.getSpeed = function() {
-		return speed;
-	}
-
-	this.setSpeed = function(_speed) {
-		speed = _speed;
-	}
-
-	this.getLife = function() {
-		return life;
-	}
-
-	this.setLife = function(_life) {
-		life = _life;
-	}
-
-	this.getBulletSpeed = function() {
-		return bulletSpeed;
-	}
-
-	this.setBulletSpeed = function(_bulletSpeed) {
-		bulletSpeed = _bulletSpeed;
-	}
-
-	this.getBulletDamage = function() {
-		return bulletDamage;
-	}
-
-	this.setBulletDamage = function(bulletDamage) {
-		bulletDamage = _bulletDamage;
-	}
+	this.speed = speed || 0;
+	this.life = life || 0;
+	this.bulletSpeed = bulletSpeed || 0;
+	this.bulletDamage = bulletDamage || 0;
 }
 
 Enemy.prototype = Object.create(Phaser.Sprite.prototype);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
@@ -15,17 +15,17 @@ var bullets;
 var bulletTime = 0;
 var bullet;
 var background;
+var weapon;
 
 function create() {
 
-    game.world.setBounds(0, 0, 1920, 600);
+    game.world.setBounds(0, 0, 800, 600);
 
-    background = game.add.tileSprite(0, 0, 1200, 600, 'starfield');
+    background = game.add.tileSprite(0, 0, 800, 600, 'starfield');
 
     ship = game.add.sprite(200, 200, 'ship');
     game.physics.enable(ship, Phaser.Physics.ARCADE);
     ship.body.collideWorldBounds = true;
-
 
     this.lifeBar = new LifeBar(game);
     this.lifeBar.display();
@@ -33,19 +33,7 @@ function create() {
     cursors = game.input.keyboard.createCursorKeys();
     game.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
 
-    bullets = game.add.group();
-    bullets.enableBody = true;
-    bullets.physicsBodyType = Phaser.Physics.ARCADE;
-
-    for (var i = 0; i < 20; i++)
-    {
-        var bullet = bullets.create(0, 0, 'bullet');
-        bullet.name = 'bullet' + i;
-        bullet.angle = 90;
-        bullet.exists = false;
-        bullet.visible = false;
-
-    }
+    weapon = new Weapon.SingleBullet(this.game);
 
     enemies = game.add.group();
     enemies.enableBody = true;
@@ -59,7 +47,7 @@ function create() {
         invader.exists = false;
         invader.visible = false;
         invader.reset(1900, 50 + i * 50);
-        invader.body.velocity.x = -invader.getSpeed();
+        invader.body.velocity.x = -invader.speed;
         invader.body.velocity.y = 0;
     }
 }
@@ -70,10 +58,9 @@ function update() {
 
     //game.camera.x += 1;
     game.physics.arcade.overlap(ship, enemies, shipCollisionHandler, null, this);
-    game.physics.arcade.overlap(bullets, enemies, bulletsCollisionHandler, null, this);
+    game.physics.arcade.overlap(weapon, enemies, bulletsCollisionHandler, null, this);
 
-    ship.body.velocity.x = 0;
-    ship.body.velocity.y = 0;
+    ship.body.velocity.set(0);
 
     if (cursors.left.isDown)
     {
@@ -95,7 +82,7 @@ function update() {
 
     if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
     {
-        fireBullet();
+        weapon.fire(ship);
     }
 }
 
@@ -105,45 +92,18 @@ function render() {
 
 }
 
-function fireBullet () {
-
-    if (game.time.now > bulletTime)
-    {
-        bullet = bullets.getFirstExists(false);
-
-        if (bullet)
-        {
-            bullet.reset(ship.x + 50   , ship.y +6);
-            bullet.body.velocity.x = 500;
-            bulletTime = game.time.now + 150;
-            var reset = function(b){
-                resetBullet(b);
-            };
-            game.time.events.add(Phaser.Timer.SECOND * 2, reset, this, bullet);
-        }
-    }
-
-}
-
-//  Called if the bullet goes out of the screen
-function resetBullet (bullet) {
-    bullet.kill();
-
-}
-
 //  Called if the bullet hits one of the enemies sprites
 function bulletsCollisionHandler (bullet, enemy) {
     bullet.kill();
 
-    var invaderLife = enemy.getLife();
-    invaderLife--;
-    enemy.setLife(invaderLife);
+    enemy.life -= bullet.damage;
 
-    if(invaderLife <= 0) {
+    if(enemy.life <= 0) {
         enemy.kill();
     }
 }
 
 function shipCollisionHandler (ship, enemy) {
+    this.lifeBar.life.updateCrop();
     enemy.kill();
 }
