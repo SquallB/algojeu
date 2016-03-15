89587d22f07ef2e7649069618f71425111d75b2a
Moumoush <mehdi.bouchagour@laposte.net>
2016-03-15 18:26:32 -0400

    changements arbre
    changements arbre

--- src/SimpleGraph/GameGraph.js
+++ src/SimpleGraph/GameGraph.js
@@ -1,6 +1,23 @@
 function GameGraph(){
-	this.nodeTypes = ["ET","ET//","OU","OU//","FIRST", "LAST" ,"OPTIONNAL","NOT"];
-	this.leafTypes = ["KILL","SURVIVE","GET_TOKEN"];
+	this.nodeTypes = ["ET","ET//","OU","OU//","FIRST", "LAST"];
+	this.leafTypes = ["kill_all","survive","get_token"];
+	this.foes = ["invader","invader2","boss"];
+	this.weapon = ["Rockets","ScaleBullet","SingleBullet","FrontAndBack","ThreeWay","EightWay","ScatterShot","Beam","SplitShot"]
+	this.token = ["shield","health","life","weapon"];
+
+
+	this.leaf = [{
+		objective : "survive",
+		"total_dammaged" : "0",
+		"vague": {
+			"numberEnemy": 3,
+		 	"type" : "invader",
+		    "life": 10,
+		    "speed": 100,
+		    "weapon": "Rockets"
+		}	
+
+		}]
 }
 
 
@@ -10,14 +27,7 @@ GameGraph.prototype.generateGraph = function(nbNodes,game){
 
 	for (var i = 1; i <= nbNodes; i++) {
 		var rndType = game.rnd.integerInRange(0, this.nodeTypes.length-1);
-		while((this.nodeTypes[rndType]==="OPTIONNAL" || this.nodeTypes[rndType]==="NOT")&& i===1){
-			rndType = game.rnd.integerInRange(0, this.nodeTypes.length-1);
-		}
-		
-
-		gameTree.addValuedNode(i,this.nodeTypes[rndType]);
-		
-
+		gameTree.addValuedNode(i,{ type :this.nodeTypes[rndType]});
 	}
 	var rootNode = gameTree.getNode(1);
 	var nextIndex = 2; 
@@ -27,10 +37,7 @@ GameGraph.prototype.generateGraph = function(nbNodes,game){
 		var rndChilds=0;
 
 		if(nextIndex<=nbNodes){
-			
-
 			rndChilds = this.rndChildNumber(type,nbNodes)
-
 			for (var j = 0; j < rndChilds; j++) {
 				
 				var toAdd = gameTree.getNode(j+nextIndex);
@@ -50,8 +57,10 @@ GameGraph.prototype.generateGraph = function(nbNodes,game){
 			var rndChilds = this.rndChildNumber(currentNode.getValue(),5);
 			for (var o = 1; o <= rndChilds; o++) {
 				var nextIndex = gameTree.getOrder()+1;
-				var rndType = game.rnd.integerInRange(0, this.leafTypes.length-1);
-				gameTree.addValuedNode(nextIndex,this.leafTypes[rndType]);
+
+				
+
+				gameTree.addValuedNode(nextIndex,this.generateRndLeaf(game));
 				gameTree.addEdge(currentNode,gameTree.getNode(nextIndex));
 
 			}
@@ -61,18 +70,58 @@ GameGraph.prototype.generateGraph = function(nbNodes,game){
 	return gameTree;
 }
 
+GameGraph.prototype.generateRndNodes = function(Tree, nbNodes){
 
+}
 GameGraph.prototype.rndChildNumber= function(type,maxNumber){
 	var rndChilds=0;
-	if(type !== "NOT" && type!== "OPTIONNAL"){
-		rndChilds = game.rnd.integerInRange(2, maxNumber);
+	
+	rndChilds = game.rnd.integerInRange(2, maxNumber);
 
-	}else{
-		rndChilds = game.rnd.integerInRange(1,maxNumber);
-	}
+	
 	return rndChilds;
 }
 
 GameGraph.prototype.displayTree = function(tree){
 	initializeInterfaceFor1Graph(tree, "levelTree", 2000, 0, 800);
+}
+
+
+
+GameGraph.prototype.generateRndLeaf = function(game){
+	var toReturn = {};
+	var rndType = game.rnd.integerInRange(0, this.leafTypes.length-1);
+	toReturn.objective = this.leafTypes[rndType];
+	toReturn.total_dammaged = "0";
+	toReturn.type = toReturn.objective;
+	if(toReturn.objective === "get_token"){
+		toReturn.token = {
+			type: this.token[game.rnd.integerInRange(0, this.token.length-1)]
+		}
+
+		switch(toReturn.token.type) {
+			case "weapon":
+				toReturn.token.value = this.weapon[game.rnd.integerInRange(0, this.weapon.length-1)];
+				break;
+			case "shield":
+				toReturn.token.value = game.rnd.integerInRange(0, 1);
+				break;
+		}
+
+
+
+	}else{
+
+		this.vague = {
+			numberEnemy : game.rnd.integerInRange(1, 10),
+			type: this.foes[game.rnd.integerInRange(0, this.foes.length-1)],
+			life: game.rnd.integerInRange(1, 20),
+			speed: game.rnd.integerInRange(10, 30),
+			weapon: this.weapon[game.rnd.integerInRange(0, this.weapon.length-1)]
+		}
+		//cas du boss
+	}
+	
+	return toReturn;
+
 }
